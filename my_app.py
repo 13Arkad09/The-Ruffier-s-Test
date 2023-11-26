eba7cd5d4ab59b1dfd7fda2c3d42affe23dd795d
13Arkad09 <152078273+13Arkad09@users.noreply.github.com>
2023-11-26 09:58:42 -0500

    v 1.1
    Class MainWin is added.

--- my_app.py
+++ my_app.py
@@ -1,1 +1,7 @@
-# напиши здесь код основного приложения и первого экрана
+class MainWin(QWidget):
+    def __init__ (self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
